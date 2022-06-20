8edc43aef427c3c374bd455b5b05cbeae745e762
Bagg6168 <nulu0265@gmail.com>
2022-06-20 18:41:51 -0400

    Initial commit

---
+++ src/main/java/net/nu11une/bobberbegone/mixin/FishingBobberEntityRendererMixin.java
@@ -0,0 +1,20 @@
+package net.nu11une.bobberbegone.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.projectile.FishingBobberEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(net.minecraft.client.render.entity.FishingBobberEntityRenderer.class)
+public class FishingBobberEntityRendererMixin {
+	@Inject(at = @At("HEAD"), method = "render(Lnet/minecraft/entity/projectile/FishingBobberEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", cancellable = true)
+	public void renderCallback(FishingBobberEntity fishingBobberEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci){
+		if((MinecraftClient.getInstance().options == null || MinecraftClient.getInstance().options.getPerspective().isFirstPerson()) && fishingBobberEntity.getHookedEntity() == MinecraftClient.getInstance().player){
+			ci.cancel();
+		}
+	}
+}
